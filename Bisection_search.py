4596fdcf90e52421713142d7215ab76c82a457df
UntitledGrub <34168073+UntitledGrub@users.noreply.github.com>
2021-07-25 23:55:00 +0100

    Update Bisection_search.py

--- Bisection_search.py
+++ Bisection_search.py
@@ -14,37 +14,41 @@ except ValueError:
 except ValueError:
     print("Raise should be a float!")
 
-def how_much_saved(n, r):
+def how_much_saved(n, r): #this function calculates how much is saved over a period of n months given a rate of saving, r, e.g. 36 months with a 0.07 monthly saving rate
     monthly_salary = (annual_salary / 12)
     current_savings = 0
-    months = 0
-    for i in range(n):
+    months = 0 #changing this value to 1 causes the program to output nothing at all
+    for i in range(n): #this increments current_savings a number of times equal to the target time input
         monthly_return = (current_savings * 0.04 / 12)
         current_savings += ((monthly_salary * r) + monthly_return)
-        if months % 6 == 0  and months != 0:    
+        if months % 6 == 0 and months != 0: #the raise is applied every 6 months   
             monthly_salary = monthly_salary + (monthly_salary * semi_annual_raise)
         months += 1
     return(current_savings)
 
 counter = 0
 down_payment = (total_cost * 0.25)
-high = 1
-low = 0
-target_time = target_time
-guess = (high + low) / 2
+high = 1 #represents a savings rate of 100%
+low = 0 #and a savings rate of 0%
+target_time = target_time #
+guess = (high + low) / 2 #we start with a guess in the middle
 
-while abs(down_payment - how_much_saved(target_time, guess)) >= 100: 
-    if 1 - guess <= 0.001:
+while abs(down_payment - how_much_saved(target_time, guess)) >= 100: #we search until we have a monthly saving rate that is within 100 
+    if 1 - guess <= 0.001: #if the guess gets too close to 1 we stop the search
         print("Down payment cannot be saved in," +' ' + str(target_time), "months!")
         break
-    elif how_much_saved(target_time, guess) > down_payment:   
+    elif how_much_saved(target_time, guess) > down_payment: #the guess is too high, so we narrow the search space to the midpoint between our current guess and low   
         high = guess
-    else:
+    else: #likewise if the guess is too low
         low = guess  
-    guess = round(((high + low) / 2), 4)
+    guess = round(((high + low) / 2), 4) #it turns out we get the same answer whether we round here or not, so the rounding is extraneous
     counter += 1
 
-if how_much_saved(target_time, guess) >= down_payment:
-    print(round(guess, 4))
-    print(counter)
+#if how_much_saved(target_time, guess) >= down_payment:
+#I realised this if condition was extraneous; when I removed it the whole program runs even with months = 1 in the function definition 
+#but the answer is still slightly too high although it LESS too high. 
+#I don't understand how the value of month in the function def makes any differnece to that if condition
+
+print(round(guess, 4))
+print(counter)
     
